a8c07569cc8a71a8b01416de348dd8390e51122c
onflapp <onflapp@gmail.com>
2023-10-30 11:55:56 +0100

    --

--- templates/resource/res-ui.js
+++ templates/resource/res-ui.js
@@ -1,3 +1,15 @@
 document.body.addEventListener('submit', function(evt) {
   localStorage.setItem('_res_ui_content_change_', ''+(new Date().getTime())); 
+  var txt = document.querySelector('textarea');
+  var x = txt.selectionStart;
+  sessionStorage.setItem('__LAST_CUR_POS', ''+x);
 });
+
+function restore_text() {
+  var x = sessionStorage.getItem('__LAST_CUR_POS');
+  var txt = document.querySelector('textarea');
+  if (x && txt) {
+    txt.selectionStart = parseInt(x);
+    txt.selectionEnd = parseInt(x);
+  }
+}
