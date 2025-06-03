de504cee7a087950023686ede794b38adf1621dc
onflapp <onflapp@gmail.com>
2025-06-03 17:18:15 +0200

    --

--- templates/resource/templates/rendertemplates.js
+++ templates/resource/templates/rendertemplates.js
@@ -4,7 +4,7 @@
   let types = [];
   
   await ctx.listAllResourceNames('.', function (path) {
-    if (path.indexOf('.') > 0) {
+    if (path.indexOf('.') > 0 && path.startsWith('_') == false) {
       let name = Utils.filename(path);
       let dir = Utils.filename_dir(path).substr(1);
       if (!types.includes(dir)) types.push(dir);
